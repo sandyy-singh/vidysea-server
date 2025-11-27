1ea17c595641603703be4619c656c85930774153
sandeep singh <sandeeprewa43@gmail.com>
2025-11-27 16:53:02 +0530

    some changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,9 @@ import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 export const Register = async (req, res) => {
-    const { name, email, password ,role} = req.body;
+    const { name, email, password, role } = req.body;
     try {
-        if (!name || !email || !password ||!role) {
+        if (!name || !email || !password || !role) {
             return res.status(400).json({ message: "fill all input fields " })
         }
         const userExist = await User.findOne({ email });
@@ -28,27 +28,30 @@ export const Register = async (req, res) => {
 export const loginUser = async (req, res) => {
     const { email, password } = req.body;
     try {
-        
+
         if (!email || !password) {
             return res.status(400).json({ message: "Fill all input fields" });
         }
-   
+
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(400).json({ message: "Invalid credentials" });
         }
-   
+
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({ message: "Invalid credentials" });
         }
- 
+
         const token = jwt.sign(
             { id: user._id, role: user.role },
             process.env.JWT_SECRET,
             { expiresIn: "1d" }
         );
-        res.cookie("token", token, { httpOnly: true, sameSite: "lax" });
+        res.cookie("token", token, {
+            httpOnly: true, secure: true,              // required for https
+            sameSite: "None",
+        });
         res.json({ role: user.role, message: "login successfully" });
     } catch (err) {
         console.error("Login Error:", err);
@@ -62,7 +65,7 @@ export const UserRole = (req, res) => {
         if (!req.user || !req.user.role) {
             return res.status(401).json({ message: "Unauthorized access" });
         }
-        res.status(200).json({ role: req.user.role ,name:req.user.name });
+        res.status(200).json({ role: req.user.role, name: req.user.name });
     } catch (error) {
         res.status(500).json({ message: "Something went wrong" });
     }
@@ -72,7 +75,8 @@ export const logOut = (req, res) => {
     try {
         res.clearCookie("token", {
             httpOnly: true,
-            sameSite: "lax",
+            secure: true,              // required for https
+            sameSite: "None",
             // secure: process.env.NODE_ENV === "production", // optional
         });
         res.status(200).json({ message: "User logged out successfully" });
@@ -81,4 +85,3 @@ export const logOut = (req, res) => {
     }
 };
 
- 
